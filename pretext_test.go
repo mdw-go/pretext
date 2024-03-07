8a949fc5ae99d16f00dbfaed86cbb8d242ae5f6d
Michael Whatcott <mike@smarty.com>
2024-03-07 16:26:59 -0700

    Emit scripture for blog post.
    https://michaelwhatcott.com/jesus-christ/the-bishop-of-our-souls/

--- pretext_test.go
+++ pretext_test.go
@@ -28,3 +28,31 @@ func Test(t *testing.T) {
 	)
 	t.Log(result)
 }
+
+const sampleText2 = `
+And the Spirit of the Lord shall rest upon him,
+    the spirit of wisdom and understanding,
+    the spirit of counsel and might,
+    the spirit of knowledge and the fear of the Lord,
+And shall make him
+      of quick understanding in the fear of the Lord,
+and he shall not judge after the sight   of his eyes,
+       neither reprove after the hearing of his ears.
+But with righteousness shall he judge the poor,
+and reprove with equity for the meek of the earth...
+     and righteousness shall be the girdle of his loins,
+     and faithfulness           the girdle of his reins.
+`
+
+func Test2(t *testing.T) {
+	t.Log(pretext.Emit(sampleText2,
+		pretext.Options.Colorize(`Spirit|spirit`, pretext.Colors.Blue()),
+		pretext.Options.Colorize(`fear of the Lord`, pretext.Colors.Red()),
+		pretext.Options.Colorize(`understanding`, pretext.Colors.LightGreen()),
+		pretext.Options.Colorize(`judge|reprove`, pretext.Colors.Maroon()),
+		pretext.Options.Colorize(`righteousness`, pretext.Colors.Green()),
+		pretext.Options.Colorize(`faithfulness`, pretext.Colors.Green()),
+		pretext.Options.Colorize(`meek|poor`, pretext.Colors.Orange()),
+		pretext.Options.Colorize(`earth`, pretext.Colors.Aqua()),
+	))
+}
